4e97ea4894f5e63621715dffda829e6e97c2fd9d
Igor Gabriel Rodrigues da Silva <130615855+IgorGabrod@users.noreply.github.com>
2025-05-09 09:14:07 -0300

    Comentando funções importantes

--- VIEW/VIEW_PROJETO/view_projeto.js
+++ VIEW/VIEW_PROJETO/view_projeto.js
@@ -1,9 +1,8 @@
-const navMenu = document.getElementById("nav-menu");
+const navMenu = document.getElementById("nav-menu"); // O menu lateral.
 const toggleMenu = document.getElementById("toggle-menu");
 const closeMenu = document.getElementById("close-menu");
 const openBtn = document.getElementById('toggle-menu'); // Botão de abrir
 const closeBtn = document.getElementById('close-menu'); // Botão de fechar
-
 
 // Abrir o menu ao clicar no ícone de hambúrguer
 toggleMenu.addEventListener("click", () => {
@@ -23,5 +22,5 @@ document.addEventListener('click', (event) => {
     // Fechar o menu se não for dentro do menu e não for o botão de abrir
     if (navMenu.classList.contains('active') && !isClickInsideMenu && !isClickOnOpenBtn) {
         navMenu.classList.remove('active');
-    }
+  }
 });
